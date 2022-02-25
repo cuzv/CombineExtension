ebbeab9db0a875d8ac475dbe6c9ab4845f018125
Shaw <cuzval@gmail.com>
2022-02-25 22:04:23 +0800

    Add CompactMap convenience methods

--- Sources/Publisher/Publishers.CompactMap.swift
+++ Sources/Publisher/Publishers.CompactMap.swift
@@ -9,6 +9,25 @@ extension Publisher {
   where Output == Wrapped? {
     compactMap { $0 ?? nil }
   }
+
+  public func with<Inserted: AnyObject>(
+    _ inserted: Inserted
+  ) -> Publishers.CompactMap<Self, (Output, Inserted)> {
+    compactMap { [weak inserted] value in
+      if let inserted = inserted {
+        return (value, inserted)
+      }
+      return nil
+    }
+  }
+
+  public func succeeding<Successor: AnyObject>(
+    _ successor: Successor
+  ) -> Publishers.CompactMap<Self, Successor> {
+    compactMap { [weak successor] _ in
+      successor
+    }
+  }
 }
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
